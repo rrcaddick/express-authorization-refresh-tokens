f2d307a369bc86e69ce11849195a82db931230e5
rrcaddick <rrcaddick@gmail.com>
2022-07-04 19:43:49 +0200

    Added serving react app

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -14,6 +15,14 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goal.routes"));
 app.use("/api/users", require("./routes/user.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "build", "index.html"));
+  });
+}
 
 app.use(errorHandler);
 
